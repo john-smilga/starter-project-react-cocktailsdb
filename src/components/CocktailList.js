2b7a38f27ffc3eae95015f4f3521cb0cd847687b
john-smilga <johnsmilga@gmail.com>
2019-12-09 08:11:30 -0800

    initial commit

---
+++ src/components/CocktailList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CocktailList() {
+  return <h1>cocktailList component</h1>;
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Navbar() {
+  return <h1>navbar component</h1>;
+}
